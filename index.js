3d5341afd24594c2719e7dcf3aa2edcafac10ecb
Elvis Momanyi <elvismomanyi23@gmail.com>
2024-01-13 11:15:28 +0300

    completed the dom modifying element lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Creating a new header element
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Setting the inner text of the new header
+newHeader.textContent = 'Elvis Momanyi is the champion';
+
+// Appending the new header to the body
+document.body.appendChild(newHeader);
